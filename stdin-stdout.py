0dcb45893055ab97d4ce9fcad24fd03446fcfc8b
Barath B K <barath2904@gmail.com>
2020-07-07 08:46:08 +0530

    updated file

--- stdin-stdout.py
+++ stdin-stdout.py
@@ -3,5 +3,9 @@ print(type(user_input))
 # input function always returns a string
 print(user_input.isdigit())
 # strip function removes leading & trailing spaces if any
-numeric_input = int(user_input.strip())
-print(numeric_input)
+if user_input.isdigit():
+    numeric_input = int(user_input.strip())
+    print(numeric_input)
+    print(type(numeric_input))
+else:
+    print("enter a valid value; Example: 123")
