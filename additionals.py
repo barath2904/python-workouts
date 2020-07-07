abcd8c6cae2d6f4a7fe846e0c2de207ac71b3d13
Barath B K <barath2904@gmail.com>
2020-07-07 07:40:47 +0530

    updated file

--- additionals.py
+++ additionals.py
@@ -1,21 +1,35 @@
 import os
+import datetime
 
+# file operations
 print(os.getcwd())
 print(os.listdir(os.getcwd()))
-pathname = os.path.join("data", "filename")
+pathname = os.path.join("basedir", "filename")
 print(pathname)
 
+path_name = "D:/"
+file_name = "sample-file-creation"
+with open(os.path.join(path_name, file_name), 'w') as new_file:
+    new_file.write("First Line \n")
+    new_file.write("Second Line \n")
+    new_file.write("continuous line1 ")
+    new_file.write("continuous line2")
+    new_file.write("\n")
+    new_file.write("End of File")
 
-import datetime
+# file & directory check
+print(os.path.isdir("D:/"))
+print(os.path.isfile("D:/sample-file-creation"))
+
+# current date & datetime
 datetime_object = datetime.datetime.now()
 print(datetime_object)
 
 date_object = datetime.date.today()
 print(date_object)
 
-from datetime import date
 # date object of today's date
-today = date.today()
+today = datetime.date.today()
 print("Current year:", today.year)
 print("Current month:", today.month)
 print("Current day:", today.day)
