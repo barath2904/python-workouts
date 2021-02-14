7558f2a6927de621434e980edddba8b672451bab
barath2904 <barath2904@gmail.com>
2021-02-14 18:40:20 +0530

    updated file

---
+++ src/00-loops-part1.py
@@ -0,0 +1,13 @@
+for i in range(3):
+    print(i)
+
+print("\n")
+
+for i in range(1, 6):
+    print(i, end="\t")
+
+print("\n")
+base_value = 0
+while base_value < 5:
+    base_value = base_value + 1
+    print(base_value)
