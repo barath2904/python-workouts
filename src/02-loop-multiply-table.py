3164a7cda36785650b8cf9b20ce9e59420291cf0
barath2904 <barath2904@gmail.com>
2021-02-20 17:39:17 +0530

    updated file

--- src/02-loop-multiply-table.py
+++ src/02-loop-multiply-table.py
@@ -1,11 +1,18 @@
+import sys
+
+
 # looping
 def mul_table(num):
     # range function returns from x to y-1 in range(x,y)
     # range function returns from 0 to n-1 in range(n)
-    for i in range(1, 11):
+    for i in range(1, 16):
         print(str(num) + ' x ' + str(i) + ' = ' + str(num*i))
 
 
 if __name__ == '__main__':
-    n = int(input("enter the number to build multiplication table for:"))
-    mul_table(n)
+    try:
+        n = int(input("enter the number to build multiplication table for:"))
+        mul_table(n)
+    except ValueError as e:
+        print(F"Enter valid value to run multiplication program.\n{e}")
+        sys.exit(1)
