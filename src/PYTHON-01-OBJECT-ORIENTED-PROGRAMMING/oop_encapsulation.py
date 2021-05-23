27091801f0b3662ecd1739fb9e043360847ef5ff
barath2904 <barath2904@gmail.com>
2021-05-23 23:32:14 +0530

    updated content

--- src/PYTHON-01-OBJECT-ORIENTED-PROGRAMMING/oop_encapsulation.py
+++ src/PYTHON-01-OBJECT-ORIENTED-PROGRAMMING/oop_encapsulation.py
@@ -5,6 +5,18 @@
 
 
 class Base:
+    def __init__(self):
+        self.amount = 100
+        self._amount = 200
+        self.__amount = 300
+
+    def print_amount(self):
+        print(self.amount)
+        print(self._amount)
+        print(self.__amount)
+
+    def set_amount(self, amount1, amount2, amount3):
+        self.amount, self._amount, self.__amount = amount1, amount2, amount3
 
     def __private(self):
         print("private value in Base")
@@ -23,13 +35,33 @@ class Derived(Base):
         print("derived private")
 
     def _protected(self):
+        self.__private()
         print("derived protected")
 
 
+b = Base()
+b.print_amount()
+print("")
+b.amount = 150
+# protected & private attributes can't be changed
+b.__amount = 250
+b.__amount = 350
+b.print_amount()
+# protected & private attributes can be changed via setter method
+print("")
+b.set_amount(150, 250, 350)
+b.print_amount()
+
+print("")
 d = Derived()
-d.public()
-print("\n\n")
+# d.public()
+print("")
 # not recommended to call protected method & hence it shows warning
 # private methods - we won't be able to call as python doesn't recognize
-# d._protected() ---> will work, but not recommended
-# d.__private()  ---> will throw error
+# d._protected()  # ---> will work, but not
+# d.__private()   # ---> will throw error
+
+# print(dir(d))  # --> prints attributes & methods of object
+# d.__private() can be called as d._Base_private() --> Name Mangling
+# Name mangling: Double underscore prefix causes python interpreter to rewrite attribute & method names
+d._Base__private()  # ---> still accessible ; Remember Encapsulation is not enforced
